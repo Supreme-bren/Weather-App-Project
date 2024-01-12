8be1dc107381670a8af4c27901f1de95a05bf817
Brennan <brennanosunkwo14@gmail.com>
2024-01-11 19:35:08 -0500

    first upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const searchBtn = document.querySelector('.search-button');
+const locationBtn = document.querySelector('.location-button');
+
+
+const getWeatherByCity = () =>{
+    let cityName = document.querySelector('.city-name').value;
+    
+}
+
+searchBtn.addEventListener('click', getWeatherByCity);
